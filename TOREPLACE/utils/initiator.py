12b6b0dd7d6b164d77da1fb7521729342310cfbd
Vladimir K <vkolesni@gmail.com>
2018-09-18 11:01:35 +0300

    Add initiator

---
+++ TOREPLACE/utils/initiator.py
@@ -0,0 +1,15 @@
+from utils.config_parser import ConfigParser
+
+__project__ = 'TOREPLACE'
+__version__ = '0.0.0'
+
+VERSION = __project__ + '-' + __version__
+
+
+class Initiator:
+    def __init__(self):
+        """
+        Initiator class to load all necessary configurations, logger and HTTP client instances
+        """
+        config_parser = ConfigParser()
+        self.config = config_parser.return_json(path_to_config='TOREPLACE/config/example.json')
