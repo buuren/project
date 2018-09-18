7ce9c4fa93ebed3899b63ee0eea15a2396a7b1d3
Vladimir K <vkolesni@gmail.com>
2018-09-18 10:39:15 +0300

    Update __main__.py

--- TOREPLACE/__main__.py
+++ TOREPLACE/__main__.py
@@ -1,6 +1,30 @@
-"""Do nothing, but document it.
+import click
+from . import Init
+from cli.options import Options
+from decorators.cli import add_options
 
-No, really, it doesn't do anything.
-"""
 
-print("foo")
+if __name__ == '__main__':
+    CONTEXT_SETTINGS = dict(
+        auto_envvar_prefix='TOREPLACE',
+        allow_extra_args=True,
+        token_normalize_func=lambda x: x.lower() if isinstance(x, str) else x
+    )
+
+    # Load configuration files
+    init = Init()
+
+    # Initate Options
+    options_object = Options()
+
+    @click.group(context_settings=CONTEXT_SETTINGS)
+    @add_options(options_object.get_click_options())
+    def cli(**kwargs):
+        options_object.set_options(**kwargs)
+
+
+    @cli.command()
+    def test():
+        pass
+
+    cli()
