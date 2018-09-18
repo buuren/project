e115a85cd5bf2c6d797ab10a61462837b1adb35e
Vladimir K <vkolesni@gmail.com>
2018-09-18 14:56:27 +0300

    Update __main__.py

--- TOREPLACE/__main__.py
+++ TOREPLACE/__main__.py
@@ -1,31 +1,5 @@
-import click
-
-from utils.initiator import Initiator
-from cli.options import Options
-from decorators.cli import add_options
+from cli.commands import cli
 
 
 if __name__ == '__main__':
-    CONTEXT_SETTINGS = dict(
-        auto_envvar_prefix='TOREPLACE',
-        allow_extra_args=True,
-        token_normalize_func=lambda x: x.lower() if isinstance(x, str) else x
-    )
-
-    # Load configuration files
-    init = Initiator()
-
-    # Initate Options
-    options_object = Options()
-
-    @click.group(context_settings=CONTEXT_SETTINGS)
-    @add_options(options_object.get_click_options())
-    def cli(**kwargs):
-        options_object.set_options(**kwargs)
-
-
-    @cli.command()
-    def test():
-        pass
-
     cli()
