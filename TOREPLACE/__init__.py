fbfa201350eb5480b78df7570069be3903a1957e
Vladimir K <vkolesni@gmail.com>
2018-09-18 11:01:53 +0300

    Update __init__.py

--- TOREPLACE/__init__.py
+++ TOREPLACE/__init__.py
@@ -1,18 +1,1 @@
 #!/usr/bin/env python
-
-from utils.config_parser import ConfigParser
-
-__project__ = 'TOREPLACE'
-__version__ = '0.0.0'
-
-VERSION = __project__ + '-' + __version__
-
-
-class Init:
-    def __init__(self):
-        """
-        Initiator class to load all necessary configurations, logger and HTTP client instances
-        """
-        config_parser = ConfigParser()
-
-        self.config = config_parser.return_json(path_to_config='config/example.json')
